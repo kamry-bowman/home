4ef3fc6805bb8951a4a4577166877e649c9adba2
Kamry Bowman <kamry.bowman@gmail.com>
2018-09-24 12:31:54 -0600

    Add meta width tag to static site

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>My page</title>
+          <meta name="viewport" content="width=device=width, initial-scale=1" />
           <link href="https://fonts.googleapis.com/css?family=Archivo+Black|Nova+Flat" rel="stylesheet"/>
           {this.props.styleTags}
         </Head>

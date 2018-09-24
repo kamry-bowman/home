527fff50ba011da13e16ae694af587c915fef61d
Kamry Bowman <kamry.bowman@gmail.com>
2018-09-24 12:35:35 -0600

    Update title on site

--- pages/_document.js
+++ pages/_document.js
@@ -13,8 +13,8 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
-          <meta name="viewport" content="width=device=width, initial-scale=1" />
+          <title>Kam Bowman</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="https://fonts.googleapis.com/css?family=Archivo+Black|Nova+Flat" rel="stylesheet"/>
           {this.props.styleTags}
         </Head>

6b13b03012a90324c41d9964ac0e2ac2d227d280
Kamry Bowman <kamry.bowman@gmail.com>
2018-10-08 22:51:38 -0600

    See previous

--- data/projects.js
+++ data/projects.js
@@ -7,8 +7,8 @@ export default [
       'CSS',
       'Javascript',
     ],
-    url: 'https://kbowman-architecture.nelify.com',
-    img: 'assets/arch-bw-300x350.gif',
+    url: 'https://kbowman-architecture.netlify.com',
+    img: 'arch-bw-300.gif',
   },
   {
     name: 'Lambda Notes',
@@ -22,6 +22,6 @@ export default [
       'Postgresql',
     ],
     url: 'https://nervous-leakey-9c1090.netlify.com',
-    img: 'assets/notes-bw-300x350.gif',
+    img: 'notes-bw-300.gif',
   },
 ];
